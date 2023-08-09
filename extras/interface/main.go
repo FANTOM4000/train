d1fdf7dd1d5b9b6c44ad80eda48e2f6df7fe8425
ThanittaT <thanittat1010@gmail.com>
2023-08-09 21:07:59 +0700

    Interface concept for Day20 since Tue8.08.2023
    Co-authored-by: FANTOM4000 <FANTOM4000@users.noreply.github.com>

--- extras/interface/main.go
+++ extras/interface/main.go
@@ -7,6 +7,8 @@ import "fmt"
 // ประกาศ interface
 type Shape interface {
 	Area() float64
+	DoubleArea() float64
+	TripleArea() float64
 }
 
 // ประกาศ struct ที่เป็น instance ของ interface Shape
@@ -14,9 +16,34 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangel struct{
+	Base float64
+	Height float64
+}
+
+func (t Triangel) Area() float64 {
+	return 0.5 *(t.Base * t.Height)
+}
+
+func (t Triangel) DoubleArea() float64 {
+	return ((t.Base*t.Height)/2)*2
+}
+func (t Triangel) TripleArea() float64 {
+	return ((t.Base*t.Height)/2)*3
+}
+
+
 // สร้าง method Area สำหรับ Circle
 func (c Circle) Area() float64 {
 	return 3.14 * c.Radius * c.Radius
+}
+
+func (c Circle) DoubleArea() float64 {
+	return (3.14 * c.Radius * c.Radius)*2
+}
+
+func (c Circle) TripleArea() float64 {
+	return (3.14 * c.Radius * c.Radius)*3
 }
 
 // ฟังก์ชันทดสอบ
@@ -24,7 +51,31 @@ func PrintArea(s Shape) {
 	fmt.Println("พื้นที่ของรูป:", s.Area())
 }
 
+type Square struct {
+	Height float64
+	Width float64
+}
+
+func(s Square) Area()float64  {
+	return s.Height*s.Width
+}
+
+func(s Square) DoubleArea()float64  {
+	return (s.Height*s.Width)*2
+}
+
+func(s Square) TripleArea()float64  {
+	return (s.Height*s.Width)*3
+}
+
+
+
 func main() {
 	circle := Circle{Radius: 5}
+	square := Square{Height: 4,Width: 5}
+	triangle := Triangel{Base: 5, Height: 8}
 	PrintArea(circle)
+	PrintArea(square)
+	PrintArea(triangle)
+
 }
